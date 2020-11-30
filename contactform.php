4291448a3fd06f028171cc9fe7349c7c55191f7f
olivierldn <33377197+olivierldn@users.noreply.github.com>
2020-11-30 12:37:58 +0000

    Add files via upload

--- contactform.php
+++ contactform.php
@@ -28,6 +28,35 @@
 
 
 
+<div class="container-contact">
+          
+          <form action="./contactform.php" method="POST">
+            <div class="form-group">
+              <label for="name" class="form-label">Your Name</label>
+              <input type="text" class="form-control" id="name" name="name" placeholder="Jane Doe" tabindex="1" required>
+            </div>
+            <div class="form-group">
+              <label for="email" class="form-label">Your Email</label>
+            <input type="email" class="form-control" id="email" name="name" placeholder="jane@doe.com" tabindex="2" required>
+          </div>
+            <div class="form-group">
+              <label for="subject" class="form-label">Subject</label>
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="Hello There!" tabindex="3" required>
+          </div>
+            <div class="form-group">
+              <label for="" class="form-label">Message</label>
+              <textarea class="form-control" rows="5" cols="50" id="message" name="message" placeholder="Enter Message..." tabindex="4"></textarea>
+          </div>
+          <div>
+            <button type="submit" class="btn">Send Message!</button>
+          </div>
+          </form>
+        </div>
+        
+      </div>
+
+
+
 
 
     ?>
